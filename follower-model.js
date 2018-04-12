208ee977fe35a2dfc96c78219b3dfe2f47cc09fb
Raiyan <mislam0525@gmail.com>
2018-04-12 14:13:45 -0400

    INitil commit

---
+++ follower-model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const timestamps = require('mongoose-timestamp');
+
+const followerSchema = mongoose.Schema({
+    username: {
+        type: String
+    },
+    
+    followers: [{
+        username: String,
+    }]
+});
+
+followerSchema.plugin(timestamps);
+module.exports = mongoose.model('followers', followerSchema);
